8889c619503bbbb777eabc3302a73856da471df0
artityas <artisuryaning2003@gmail.com>
2026-04-24 23:09:06 +0700

    Create Node.java

---
+++ Data Structure & Algorithm/TA1-LAB/Node.java
@@ -0,0 +1,11 @@
+// Kelas Node untuk Linked List
+class Node {
+    String nama;   // Menyimpan nama pelanggan
+    Node next;     // Pointer ke node berikutnya
+
+    // Constructor
+    Node(String nama) {
+        this.nama = nama;
+        this.next = null;
+    }
+}
